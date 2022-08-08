3af393e0cd25a226488f7bdf946e4cbe0f3b65c9
LeHoang510 <lehoangnguyen510@gmail.com>
2022-08-08 05:46:20 +0200

    Finished sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,38 @@
 module SessionsHelper
+  def logged_in?
+    !current_user.nil?
+  end
+  def login(user)
+    session[:user_id]=user.id
+  end
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user||=User.find_by(id: user_id)
+    elsif(user_id= cookies.signed[user_id])
+      user= User.find_by(id: user_id)
+      if user&.authenticated?(cookies[:remember_token])
+        login user
+        @current_user=user
+      end
+    end
+  end
+
+  def logout
+    forget current_user
+    @current_user=nil
+    session[:user_id]=nil
+  end
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id]=user.id
+    cookies.permanent[:remember_token]=user.remember_token
+  end
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  def store_location
+    session[:url_stored]=request.original_url if request.get?
+  end
 end
